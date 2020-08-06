9ed05bd07126ed92a6534bea76536ca9ea292df5
Leonardo J. Ramirez <lramirez.army@gmail.com>
2020-08-06 00:42:08 -0400

    added fullfillment order api logic"

--- routes/orders.js
+++ routes/orders.js
@@ -24,10 +24,21 @@ router.post('/fullfilled/:id', async (req, res) => {
   if (!order) return res.status(400).send('Order not found.');
 
   await order.updateOne({ isFullfilled: true });
-  // await User.findOneAndDelete({ 'orders._id': req.params.id }); //update order status to Fullfilled
-  // user.save();
+  const user = await User.findOneAndUpdate(
+    {
+      orders: { $elemMatch: { _id: req.params.id } }
+    },
+    {
+      'orders.$.isFullfilled': true
+    },
+    { new: true }
+  );
 
-  res.send();
+  const user_order = user.orders.find((order) => order._id == req.params.id);
+
+  res.send({
+    order_status: user_order.isFullfilled
+  });
 });
 
 module.exports = router;
